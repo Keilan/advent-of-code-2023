c2f92de1e638b84e804ccf2a696848eac0670060
Keilan Scholten <keilan.scholten@outlook.com>
2023-12-08 20:57:16 -0700

    Day8

---
+++ Days/Day8/Day8.cs
@@ -0,0 +1,101 @@
+using AdventOfCode2023.Utilities;
+
+namespace AdventOfCode2023.Days;
+
+public static class Day8
+{
+
+
+    static void ReadInput(string[] input, out List<char> directions, out Dictionary<string, (string left, string right)> map)
+    {
+        directions = [];
+        map = [];
+        foreach (string line in input)
+        {
+            if (string.IsNullOrEmpty(line)) 
+            { 
+                continue; 
+            }
+
+
+            if (line.Contains(" = "))
+            {
+                List<string> split = line.Split(" = ").ToList();
+                string position = split[0];
+
+                List<string> paths = split[1].Replace("(", "").Replace(")", "").Split(", ").ToList();
+                map[position] = (paths[0], paths[1]);
+
+            }
+
+            // Directions
+            else
+            {
+                directions = line.ToCharArray().ToList();
+            }
+        }
+    }
+
+    public static void Solution(string[] input)
+    {
+        // Read the map
+        List<char> directions;
+        Dictionary<string, (string left, string right)> map;
+        ReadInput(input, out directions, out map);
+
+        int steps = 0;
+        string position = "AAA";
+        int directionIndex = 0;
+        while (position != "ZZZ")
+        {
+            steps++;
+            (string left, string right) paths = map[position];
+
+            if (directions[directionIndex] == 'L')
+            {
+                position = paths.left;
+            }
+            else
+            {
+                position = paths.right;
+            }
+
+            directionIndex++;
+            directionIndex %= directions.Count;
+        }
+
+        Console.WriteLine($"Part 1: {steps}");
+
+        // Get starting nodes
+        List<string> starts = map.Keys.Where(k => k.Last() == 'A').ToList();
+        List<int> cycleLengths = [];
+
+        foreach (string start in starts) 
+        {
+            steps = 0;
+            position = start;
+            directionIndex = 0;
+            while (position.Last() != 'Z')
+            {
+                steps++;
+                (string left, string right) paths = map[position];
+
+                if (directions[directionIndex] == 'L')
+                {
+                    position = paths.left;
+                }
+                else
+                {
+                    position = paths.right;
+                }
+
+                directionIndex++;
+                directionIndex %= directions.Count;
+            }
+            cycleLengths.Add(steps);
+        }
+
+        long lcm = MathUtils.LeastCommonMultiple(cycleLengths.Select(l => (long)l).ToArray());
+        Console.WriteLine($"Part 2: {lcm}");
+    }
+}
---
+++ Utilities/Math.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AdventOfCode2023.Utilities
+{
+    public static class MathUtils
+    {
+        /// <summary>
+        /// Computes the greatest common divisor using the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long GreatestCommonDivisor(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return System.Math.Abs(a);
+        }
+
+        public static long GreatestCommonDivisor(params long[] numbers)
+        {
+            long gcd = GreatestCommonDivisor(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                gcd = GreatestCommonDivisor(gcd, numbers[i]);
+            }
+            return gcd;
+        }
+
+        public static long LeastCommonMultiple(long a, long b)
+        {
+            return (a / GreatestCommonDivisor(a, b)) * b;
+        }
+
+        public static long LeastCommonMultiple(params long[] numbers)
+        {
+            long lcm = LeastCommonMultiple(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                lcm = LeastCommonMultiple(lcm, numbers[i]);
+            }
+            return lcm;
+        }
+    }
+}
